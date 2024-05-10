742edbfcf05fe41c48aa0e4576d67ba986c04ea3
wenyuanchuang <138634041+wenyuanchuang@users.noreply.github.com>
2024-05-10 08:56:55 +0800

    Update views.py

--- IT_help/views.py
+++ IT_help/views.py
@@ -77,7 +77,7 @@ def findStock(number):
                 else:
                     eachResponse = maps[i] + ": " + data["msgArray"][0][columns[i]] + '\n'
                 totalResponse += eachResponse
-            eachResponse = "漲跌百分比: " + count_per(data["msgArray"][0]['z'], data["msgArray"][0]['y']) + '\n輸入其他股票代碼以繼續查詢。\n\n提醒您，若無即時回應代表伺服器正在重啟，請等1分鐘之後重新傳送股票代碼'
+            eachResponse = "漲跌百分比: " + count_per(float(data["msgArray"][0]['z']), float(data["msgArray"][0]['y'])) + '\n輸入其他股票代碼以繼續查詢。\n\n提醒您，若無即時回應代表伺服器正在重啟，請等1分鐘之後重新傳送股票代碼'
          
             totalResponse += eachResponse
             return totalResponse
